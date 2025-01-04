af6dafc2ece22d0a669397c825da1deec9044673
Michael Lee <mlee@Michaels-Macbook-Pro.local>
2025-01-03 23:00:18 -0700

    hello-world app

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"log"
+	"fmt"
+	"net/http"
+	"io"
+	"errors"
+	"os"
 )
 
-func main() {
-	log.Println("Hello World")
+func getHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("recieved / request\n");
+	io.WriteString(w, "This is hello world")
 }
+
+func main() {
+	http.HandleFunc("/", getHello);
+	err := http.ListenAndServe(":8080", nil);
+	if errors.Is(err, http.ErrServerClosed) {
+		fmt.Printf("server close\nd")
+	} else if err != nil {
+		fmt.Printf("error starting server\n", err);
+		os.Exit(1)
+	}
+}
+
